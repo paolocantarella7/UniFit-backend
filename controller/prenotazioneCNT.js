f7bb95cadd55ea82c265f4413d0bd41318511cd5
scafag <g.scafa@libero.it>
2022-01-10 18:22:46 +0100

    [PRZ] - refs #26,#27,#28,#29,#30,#31 <Aggiunti RF>

--- controller/prenotazioneCNT.js
+++ controller/prenotazioneCNT.js
@@ -1,8 +1,11 @@
 let Utente = require("../model/Utente");
 let RichiestaTesseramento = require("../model/Richiesta_tesseramento");
 let Prenotazione = require("../model/Prenotazione");
+let Chiusura = require("../model/Chiusura");
 let Struttura = require("../model/Struttura");
 let { Sequelize } = require("../singleton/singleton");
+let senderEmail = require("../utils/sendEmail");
+let { validationResult } = require("express-validator");
 
 /**
  * Nome metodo: getPrenotazioniByUtente
@@ -30,6 +33,247 @@ exports.getPrenotazioniByUtente = async (req, res) =>{
     .catch((err) =>{
         res.status(400).json({code: 400, msg: err, success: false});
     })
-  }
+  };
 
 
+
+/**
+ * Nome metodo: getFasceOrarie
+ * Descrizione: Metodo che permette di ottenere le fasce orarie prenotabili
+ * Parametri: idStruttura
+ * Return: Codice, lista fasce, boolean true/false in base alla riuscita dell'operazione
+ * Autore : Giuseppe Scafa
+ */
+
+exports.getFasceOrarie = async(req, res) =>{
+    let listaFasce;
+    await Struttura.findByPk(req.query.idStruttura, {
+        attributes:["oraInizioMattina", "oraFineMattina", "oraInizioPomeriggio", "oraFinePomeriggio", "durataPerFascia"]
+    }).then((result) =>{
+        if(result){
+            listaFasce = getListaFasce(result.oraInizioMattina, result.oraFineMattina, result.oraInizioPomeriggio, result.oraFinePomeriggio, result.durataPerFascia);
+            res.status(200).json({code: 200, listaFasce: listaFasce, success:true});
+        }
+        else
+         res.status(400).json({code:400, msg:"Struttura non trovata", success: false});
+    }).catch((err) =>{
+        res.status(400).json({code:400, msg:err, success: false});
+    });
+};
+
+
+/**
+ * Nome metodo: effettuaPrenotazione
+ * Descrizione: Metodo che permette di effettuare una prenotazione
+ * Parametri: informazioni prenotazione
+ * Return: Codice, messaggio, boolean true/false in base alla riuscita dell'operazione
+ * Autore : Giuseppe Scafa
+ */
+
+exports.effettuaPrenotazione = async(req, res) =>{
+    let erroriValidazione = validationResult(req);
+    let capacita, prezzo, postiOccupati;
+  if (!erroriValidazione.isEmpty()) {
+    return res.status(400).json({ code: 400, error: erroriValidazione.array(), success:false});}
+    
+    await Struttura.findByPk(req.query.idStruttura, {
+        attributes:["capacitaPerFascia", "prezzoPerFascia"]
+    }).then((result) =>{
+        if(result){
+            capacita = result.capacitaPerFascia;
+            prezzo = result.prezzoPerFascia;
+        }
+        else
+           return res.status(400).json({code: 400, msg:"Struttura non esistente", success:false});
+
+    }).catch((err) =>{
+        return res.status(400).json({code: 400, msg:err, success:false});
+    });
+    
+    let fasciaOraria = req.query.fascia.split("-");
+    await Prenotazione.count({where: {
+        struttura: req.query.idStruttura,
+        dataPrenotazione: req.query.dataPrenotazione,
+        oraInizio: fasciaOraria[0],
+        oraFine: fasciaOraria[1]
+    }}).then((result) =>{
+        postiOccupati = result;
+    }).catch((err) =>{
+        return res.status(400).json({code: 400, msg:err, success:false});
+    });
+
+    if(postiOccupati < capacita){
+        let newPrenotazione = {
+            dataPrenotazione:req.query.dataPrenotazione,
+            oraInizio: fasciaOraria[0],
+            oraFine: fasciaOraria[1], 
+            totalePagato: prezzo,
+            utente: req.query.idUtente,
+            struttura: req.query.idStruttura
+        };
+        await Prenotazione.create(newPrenotazione)
+        .then((result) =>{
+            return res.status(200).json({code: 200, msg:"Operazione effettuata con successo", success:true});
+        }).catch((err) =>{
+            return res.status(400).json({code: 400, msg:err, success:false});
+        })
+    }
+    
+
+};
+
+
+
+
+/**
+ * Nome metodo: getListaFasce
+ * Descrizione: Metodo che permette di ottenere la lista delle fasce orarie prenotabili
+ * Parametri: orari di apertura di una struttura
+ * Return: lista di fasce orarie prenotabili
+ * Autore : Giuseppe Scafa
+ */
+
+let getListaFasce = (oraInizioMattina, oraFineMattina, oraInizioPomeriggio, oraFinePomeriggio, durataPerFascia) =>{
+    let listaFasce = [];
+    let inizioFascia, fineFascia;
+    fineFascia = oraInizioMattina;
+        do{
+            
+            inizioFascia = fineFascia.slice(0,5);
+            fineFascia = (parseInt(fineFascia.slice(0,2)) + durataPerFascia).toString().concat(":00");
+           
+            if(fineFascia > oraFineMattina.slice(0,5))
+                fineFascia = oraFineMattina.slice(0,5);
+            listaFasce.push(inizioFascia + "-" + fineFascia);
+            
+        }while(fineFascia != oraFineMattina.slice(0,5));
+        
+
+        fineFascia = oraInizioPomeriggio;
+        do{
+            
+            inizioFascia = fineFascia.slice(0,5);
+            fineFascia = (parseInt(fineFascia.slice(0,2)) + durataPerFascia).toString().concat(":00");
+           
+            if(fineFascia > oraFinePomeriggio.slice(0,5))
+                fineFascia = oraFinePomeriggio.slice(0,5);
+            listaFasce.push(inizioFascia + "-" + fineFascia);
+
+            
+        }while(fineFascia != oraFinePomeriggio.slice(0,5));
+        return listaFasce;
+}
+
+
+
+/**
+ * Nome metodo: modificaPrenotazione
+ * Descrizione: Metodo che permette di modificare una prenotazione
+ * Parametri: nuovi dati prenotazione, idStruttura e idPrenotazione
+ * Return: Codice, messaggio, boolean true/false in base alla riuscita dell'operazione
+ * Autore : Giuseppe Scafa
+ */
+
+exports.modificaPrenotazione = async (req, res) =>{
+    let capacita, postiOccupati;
+    await Struttura.findByPk(req.query.idStruttura, {
+        attributes:["capacitaPerFascia"]
+    }).then((result) =>{
+        if(result){
+            capacita = result.capacitaPerFascia;
+            
+        }
+        else
+           return res.status(400).json({code: 400, msg:"Struttura non esistente", success:false});
+
+    }).catch((err) =>{
+        return res.status(400).json({code: 400, msg:err, success:false});
+    });
+
+    let fasciaOraria = req.query.fascia.split("-");
+    await Prenotazione.count({where: {
+        struttura: req.query.idStruttura,
+        dataPrenotazione: req.query.dataPrenotazione,
+        oraInizio: fasciaOraria[0],
+        oraFine: fasciaOraria[1]
+    }}).then((result) =>{
+        postiOccupati = result;
+    }).catch((err) =>{
+        return res.status(400).json({code: 400, msg:err, success:false});
+    });
+
+    if(postiOccupati < capacita){
+        await Prenotazione.update({
+            oraInizio: fasciaOraria[0],
+            oraFine: fasciaOraria[1]
+        }, {
+            where:{
+                idPrenotazione: req.query.idPrenotazione
+            }
+        }).then((result) =>{
+            return res.status(200).json({code: 200, msg:"Modifica effettuata con successo!", success:true});
+        }).catch((err) =>{
+            return res.status(400).json({code: 400, msg:err, success:false});
+        })
+    }
+    else{
+        return res.status(400).json({code: 400, msg:"Fascia oraria piena!", success:false});
+    }
+}
+
+/**
+ * Nome metodo: cancellaPrenotazione
+ * Descrizione: Metodo che permette di cancellare una prenotazione
+ * Parametri: email utente, idPrenotazione e importo
+ * Return: Codice, messaggio, boolean true/false in base alla riuscita dell'operazione
+ * Autore : Giuseppe Scafa
+ */
+
+exports.cancellaPrenotazione = async (req, res) =>{
+    let erroriValidazione = validationResult(req);
+    let reslt;
+    if (!erroriValidazione.isEmpty()) {
+        return res.status(400).json({ code: 400, error: erroriValidazione.array(), success:false});}
+    try{
+        reslt = await Prenotazione.findByPk(req.query.idPrenotazione, {
+            attributes:["totalePagato", "dataPrenotazione"]
+        })
+    }
+    catch(err){
+        return res.status(400).json({ code: 400, msg: err, success:false});
+    }
+    let dataP = new Date(reslt.dataPrenotazione);
+    let dataO = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+    if(dataP.toISOString().substring(0,10) != dataO.toISOString().substring(0,10)){
+        effettuaRimborso(req.query.email, reslt.totalePagato);
+    }
+
+    await Prenotazione.destroy({
+        where:{
+            idPrenotazione: req.query.idPrenotazione
+        }
+    }).then((result) =>{
+        if(result)
+            return res.status(200).json({code: 200, msg:"Cancellazione avvenuta con successo!", success: true});
+    }).catch((err)=>{
+        return res.status(400).json({code: 400, msg:err, success:false});
+    });
+}
+
+/**
+ * Nome metodo: effettuaRimborso
+ * Descrizione: Metodo che genere una email di rimborso
+ * Parametri: email utente e importo
+ * Return: Codice, messaggio, boolean true/false in base alla riuscita dell'operazione
+ * Autore : Giuseppe Scafa
+ */
+
+let effettuaRimborso = async (email, importo) =>{
+    try{
+        await senderEmail.sendRimborsoEmail(email, importo);
+        
+    }
+    catch(err){
+        console.log(err);
+    }
+}
--- routes/prenotazioneRoutes.js
+++ routes/prenotazioneRoutes.js
@@ -1,7 +1,53 @@
 let express = require("express");
 let router = express.Router();
 let prenotazioneCNT = require("../controller/prenotazioneCNT");
+let { param } = require("express-validator");
+let Chiusura = require("../model/Chiusura");
+let { query } = require("express-validator");
+
+let validazione = {
+    email: /[a-zA-Z0-9\._-]+[@][a-zA-Z0-9\._-]+[.][a-zA-Z]{2,6}/,
+    nome: /[A-zÀ-ù ‘-]{2,45}$/,
+    numeroCarta: /^([0-9]{16})$/,
+    cvvCarta: /^[0-9]{3,4}$/,
+    data: /^\d{4}(-\d\d(-\d\d(T\d\d:\d\d(:\d\d)?(\.\d+)?(([+-]\d\d:\d\d)|Z)?)?)?)?$/,
+    dataLimite: new Date().toDateString(),
+  };
 
 router.get("/prenotazioniUtente", prenotazioneCNT.getPrenotazioniByUtente);
+router.get("/getFasce", prenotazioneCNT.getFasceOrarie);
 
+router.get("/effettuaPrenotazione", [
+    query("intestatarioCarta")
+    .matches(validazione.nome)
+    .withMessage("Formato nome non valido"),
+    query("numeroCarta")
+    .matches(validazione.numeroCarta)
+    .withMessage("Formato numero carta non valido"),
+    query("cvvCarta")
+    .matches(validazione.cvvCarta)
+    .withMessage("Formato CVV non valido"),
+    query("dataPrenotazione")
+    .custom(async (value) =>{
+        
+        return await Chiusura.count({
+            where:{
+                struttura: 1,
+                dataChiusura:value
+            }
+        }).then((result) =>{
+            if(result>0)
+                throw new Error("Struttura chiusa nel giorno selezionato!");
+        })
+    })
+
+], prenotazioneCNT.effettuaPrenotazione);
+
+router.get("/modificaPrenotazione", prenotazioneCNT.modificaPrenotazione);
+router.get("/cancellaPrenotazione", [
+    query("email")
+    .matches(validazione.email)
+    .withMessage("Formato email errato")
+],
+prenotazioneCNT.cancellaPrenotazione);
 module.exports = router;
