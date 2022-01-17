65a1f6da7c4a7e3b5993a41413f208f37296b356
mattdr5 <dellarocca16@gmail.com>
2022-01-17 10:15:28 +0100

    [PRU] - refs #88 <Modificato url per recupero password da inviare come email>

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,5 +1,8 @@
 let nodemailer = require("nodemailer");
 
+let host = "http://localhost:3000";
+let emailUniFit = "unifit2022@gmail.com"; //inserire email UniFit gmail
+let passwordUniFit = "UniFitFit@2022@"; //inserire password UniFit gmail
 
 /**
  * Nome metodo: sendEmailWithToken
@@ -13,8 +16,8 @@ exports.sendEmailWithToken = (email, token) => {
     let mail = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: '', // Inserire email UniFit
-            pass: '' // Inserire password UniFit
+            user: emailUniFit, // Inserire email UniFit
+            pass: passwordUniFit // Inserire password UniFit
         }
     });
  
@@ -24,7 +27,7 @@ exports.sendEmailWithToken = (email, token) => {
         subject: 'Reset Password Link - UniFit.it',
         html: '<p>Gentile Utente, <br>' 
         +'è stato richiesto un recupero della password, '+
-        'usi questo <a href="http://localhost:3000/user/reset-password/' + token + '">link</a> per resettare la password'+
+        'usi questo <a href='+host+'/recovery/' + token + '">link</a> per resettare la password'+
         ' altrimenti <b>ignori</b> questa e-mail. <br>'+
         '<br>Cordiali saluti, <br> il team UniFit.</p>'
  
