bf2bc92f6b03ade0c56f2dafe4c8daa9abcf7505
mattdr5 <dellarocca16@gmail.com>
2022-01-17 10:16:57 +0100

    [PRU] -<Aggiornamento campi email e password>

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,8 +1,8 @@
 let nodemailer = require("nodemailer");
 
 let host = "http://localhost:3000";
-let emailUniFit = "unifit2022@gmail.com"; //inserire email UniFit gmail
-let passwordUniFit = "UniFitFit@2022@"; //inserire password UniFit gmail
+let emailUniFit = ""; //inserire email UniFit gmail
+let passwordUniFit = ""; //inserire password UniFit gmail
 
 /**
  * Nome metodo: sendEmailWithToken
